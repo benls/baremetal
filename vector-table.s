cb403fe15b6514b55f8ee9386a3f81d00b384803
Ben Smith <ben8402@gmail.com>
2016-04-07 14:41:33 -0700

    fixed interrupts

--- vector-table.s
+++ vector-table.s
@@ -2,9 +2,9 @@
 .globl irq_handler
 .globl vector_table_init
 
-.section ".bss"
-.balign 64
-irq_stack: .skip 512
+.bss
+irq_lr_tmp: .word
+irq_spsr_tmp: .word
 
 .text
 .balign 64
@@ -34,37 +34,49 @@ fiq:
 b fiq
 
 irq:
-push {r0-r3,r12,lr}
-/* Switch to SVC mode */
-//TODO: just us a constant
-mrs r0, cpsr
-bic r0, r0, #0x1f
-orr r0, r0, #0x13
-msr cpsr, r0
+/* Save previous mode info */
+ldr r13, =irq_lr_tmp
+str lr, [r13,#0]
+mrs lr, spsr
+str lr, [r13,#4]
+/* Switch to SVC mode with irq disabled*/
+ldr lr, =0x193
+msr cpsr, lr
 /* push registers not saved by irq_handler*/
-push {lr}
+push {r0-r3,r12,lr}
+/* push svc psr and svc lr */
+ldr r2, =irq_lr_tmp
+ldr r0, [r2]
+ldr r1, [r2,#4]
+push {r0,r1}
 /* TODO: re-align stack on 64b boundary */
-blX irq_handler
-pop {lr}
-//TODO: just us a constant
-mrs r0, cpsr
-bic r0, r0, #0x1f
-orr r0, r0, #0x12
+/* call handler */
+blx irq_handler
+/* disable interrupts */
+ldr r0, =0x193
 msr cpsr, r0
+/* Save psr and lr */
+ldr r2, =irq_lr_tmp
+pop {r0,r1}
+str r0, [r2]
+str r1, [r2,#4]
 pop {r0-r3,r12,lr}
+/* Switch back to irq mode */
+//TODO: don't corrupt r12
+ldr r12, =0x192
+msr cpsr, r12
+/* Restore previous mode info */
+ldr r13, =irq_lr_tmp
+ldr lr, [r13,#4]
+msr spsr, lr
+ldr lr, [r13]
+/* Switch back to SVC mode */
 subs pc, lr, #4
 
 vector_table_init:
+ldr r1, =0x192
 mrs r0, cpsr
-mov r1, r0
-//TODO: constant
-bic r0, r0, #0x1f
-orr r0, r0, #0x12
-orr r0, r0, #0x1c0
-msr cpsr, r0
-ldr r13, =irq_stack
-add r13, r13, #0x1fc
 msr cpsr, r1
+msr cpsr, r0
 bx lr
-
 
